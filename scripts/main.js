ec206eb43bd2e7f66fd9d8e665cd668929dd4726
Kasia Goj <katarzyna.goj@fundingbox.com>
2020-02-23 13:07:35 +0100

    create display message function

--- scripts/main.js
+++ scripts/main.js
@@ -22,15 +22,36 @@ function renderPhotos (photos){
     });
 }
 
+function displayMessage () {
+    const $photos = document.querySelector('#photos');
+    const $message = document.createElement('div');
+    $message.classList.add('message');
+    
+    const $header =document.createElement('div');
+    $header.classList.add('message-header');
+    $header.textContent = 'Message to the user';
+
+    const $body = document.createElement('div');
+    $body.classList.add('message-body');
+    $body.textContent = "Gallery is empty because we don't have any photos";
+
+    $message.append($header);
+    $message.append($body);
+    $photos.append($message);
+    
+}
+
+
 //declaration
 function main() {
     console.log('main function works');
     const photos = [
-        "https://i.picsum.photos/id/1036/200/300.jpg",
-        "https://i.picsum.photos/id/1035/200/300.jpg",
-        "https://i.picsum.photos/id/1033/200/300.jpg",
+    //     "https://i.picsum.photos/id/1036/200/300.jpg",
+    //     "https://i.picsum.photos/id/1035/200/300.jpg",
+    //     "https://i.picsum.photos/id/1033/200/300.jpg",
     ];
-    renderPhotos(photos);
+    // renderPhotos(photos);
+    displayMessage();
 
 }
 
