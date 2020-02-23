b849e894f98bcf87827224756b4723529597f6d7
Kasia Goj <katarzyna.goj@fundingbox.com>
2020-02-23 12:41:51 +0100

    split to smaller functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,27 @@
+function renderPhoto (photo){
+
+    console.log('photo', photo);
+    //create the html image element
+    const $photo = document.createElement('img');
+    $photo.src = photo;
+    //render process
+    const $photos = document.querySelector('#photos');
+    $photos.append($photo);
+}
+
+function renderPhotos (photos){
+   // for (let i = 0; i < photos.length; i++) {
+     //   const photo = photos[i];
+       // renderPhoto(photo);
+        //longer version of the above line would be: $photo.setAttribute('src', photo);
+
+    //}
+    photos.forEach(function(photo){
+        renderPhoto(photo);
+
+    });
+}
+
 //declaration
 function main() {
     console.log('main function works');
@@ -6,19 +30,8 @@ function main() {
         "https://i.picsum.photos/id/1035/200/300.jpg",
         "https://i.picsum.photos/id/1033/200/300.jpg",
     ];
-    console.log(photos);
-    for (let i = 0; i < photos.length; i++) {
-        const photo = photos[i];
-        console.log('photo', i, photo);
-        //create the html image element
-        const $photo = document.createElement('img');
-        $photo.src = photo;
-        //render process
-        const $photos = document.querySelector('#photos');
-        $photos.append($photo);
-        //longer version of the above line would be: $photo.setAttribute('src', photo);
+    renderPhotos(photos);
 
-    }
 }
 
 //function invoke
