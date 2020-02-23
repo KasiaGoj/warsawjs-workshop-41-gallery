dae171469d58b64a7fed5a373ebb82a547c57e37
Kasia Goj <katarzyna.goj@fundingbox.com>
2020-02-23 14:11:11 +0100

    added if statement

--- scripts/main.js
+++ scripts/main.js
@@ -22,36 +22,54 @@ function renderPhotos (photos){
     });
 }
 
-function displayMessage () {
+function displayMessage (message) {
     const $photos = document.querySelector('#photos');
     const $message = document.createElement('div');
     $message.classList.add('message');
     
-    const $header =document.createElement('div');
-    $header.classList.add('message-header');
-    $header.textContent = 'Message to the user';
+    // const $header =document.createElement('div');
+    // $header.classList.add('message-header');
+    // $header.textContent = 'Message to the user';
 
-    const $body = document.createElement('div');
-    $body.classList.add('message-body');
-    $body.textContent = "Gallery is empty because we don't have any photos";
+    // const $body = document.createElement('div');
+    // $body.classList.add('message-body');
+    // $body.textContent = "Gallery is empty because we don't have any photos";
 
-    $message.append($header);
-    $message.append($body);
+    // $message.append($header);
+    // $message.append($body);
+    // $photos.append($message);
+
     $photos.append($message);
+    $message.innerHTML = `
+        <div class="message-header">
+        Message
+        </div>
+        <div class="message-body">
+            ${message}
+        </div>
+        `;
     
 }
 
+function displayEmptyGalleryMessage (){
+    displayMessage("Gallery is empty. No pictures. Sorry");   
+}
 
 //declaration
 function main() {
     console.log('main function works');
     const photos = [
-    //     "https://i.picsum.photos/id/1036/200/300.jpg",
-    //     "https://i.picsum.photos/id/1035/200/300.jpg",
-    //     "https://i.picsum.photos/id/1033/200/300.jpg",
+        "https://i.picsum.photos/id/1036/200/300.jpg",
+        "https://i.picsum.photos/id/1035/200/300.jpg",
+        "https://i.picsum.photos/id/1033/200/300.jpg",
     ];
-    // renderPhotos(photos);
-    displayMessage();
+    const isGalleryEmpty = (photos.length === 0);
+    if (isGalleryEmpty) {
+        displayEmptyGalleryMessage();  
+    }
+    else {
+          renderPhotos(photos);
+    }
 
 }
 
